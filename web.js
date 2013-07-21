d5dbf501d6214f694d1ded861260f44bc887ba83
daniel polonsky <danielpolonsky132@gmail.com>
2013-07-21 20:51:55 +0000

    updated web.js

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(fs.readfilesync('index.html'));
+  response.send(fs.readfilesync("index.html"));
 });
 
 var port = process.env.PORT || 5000;
