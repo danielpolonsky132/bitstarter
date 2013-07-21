ae7ef23d978a4ac7bb7330f0ae2fec0a1be694de
daniel polonsky <danielpolonsky132@gmail.com>
2013-07-21 21:02:10 +0000

    updated web.js again

--- web.js
+++ web.js
@@ -1,14 +1,12 @@
-var fs = require('fs');
 var express = require('express');
+var fs = require('fs');
+
+var data = fs.readFileSync("index.html", "utf-8");
 
 var app = express.createServer(express.logger());
-
-var data = fs.readfilesSync("index.html","utf-8");
-
 app.get('/', function(request, response) {
   response.send(data);
 });
-
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
